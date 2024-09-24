dfe86ea049b7e90aee0e9fdadf2d9dcec1c13af1
Daris A Nevil <dnevil@intrusion.com>
2024-09-24 03:16:32 +0000

    Bug fixes.

--- python/Command.py
+++ python/Command.py
@@ -25,20 +25,45 @@
 
 class Command:
 
+    # Class variable containing the list of all registered commands.
+    c_command_list = list()
+
     # Define a command
     # @param p_word_list The list of words for the command
     # @param p_desc Text that describes the command, output of help()
     # @param p_func A function that will execute the command
-    #@param p_command_list A list that this command will be appended to
     #
-    def __init__(self, p_word_list, p_desc, p_func, p_command_list):
+    def __init__(self, p_word_list, p_desc, p_func):
         self.m_word_list = p_word_list
         self.m_desc = p_desc
         self.m_func = p_func
-        self.m_command_list = p_command_list
-        p_command_list.append(self)
-        if (!self.m_func):
+        Command.c_command_list.append(self)
+        if (not self.m_func):
             raise Exception('Undefined Command function 02407231742')
+
+
+    # Create a list of help strings describing the registered commands
+    # @returns A list of strings
+    #
+    @staticmethod
+    def Help():
+        help_list = list()
+        for cmd in Command.c_command_list:
+            s = cmd.m_word_list[0]
+            s += ": "
+            s += cmd.m_desc
+            help_list.append(s)
+        return help_list
+
+
+    @staticmethod
+    def ParseAndExec(p_input_words):
+        for cmd in Command.c_command_list:
+            (cmd_match, func_result, result_list) = cmd.parse_and_exec(p_input_words)
+            if (cmd_match):
+                return func_result, result_list
+        inv_cmd = ["Invalid command"]
+        return False, inv_cmd
 
 
     # Compare the input to the command, and if a match, execute the associated function.
@@ -46,56 +71,28 @@ class Command:
     # @param p_words_len The number of strings in p_words.
     # @param p_func_result The result of the function, if called
     # @param p_output Reference to a string to receive user-text from the command.
-    # @returns (cmd_match, func_result, func_str), where
+    # @returns (cmd_match, func_result, result_lis), where
     #          cmd_match is true if this command matches
     #          func_result is the result of the function
-    #          func_str is the string result from the function
+    #          result_list is a list of strings from the function
     #
-    def parse_and_exec(self, p_input_words)
+    def parse_and_exec(self, p_input_words):
 
         # Test if the input words invoke this command
         if (len(p_input_words) != len(self.m_word_list)):
-            return False, False, ""
+            empty = list()
+            return False, False, empty
 
-        words = []
-        macros = []
         for i in range(len(p_input_words)):
-            # Test for a macro to be parsed
-            if (p_input_words[i] == '$'):
-                # Let the m_func expand the macro, if called
-                macros.append(p_input_words[i]);
-                continue;
+            # '$' represents a variable, skip it
+            if (self.m_word_list[i] == '$'):
+                continue
 
-            if (p_input_words[i] != self._word_list[i])
+            if (p_input_words[i] != self.m_word_list[i]):
                 return False, False, ''
 
-            # Save the list of non-macro constant words
-            words.append(p_input_words[i])
-
         # Execute the function and provide the output value
-        return self.m_func(words, macros)
+        (func_result, result_list) = self.m_func(p_input_words)
+        return True, func_result, result_list
 
-
-    # Build text that describes this command
-    # @returns The help string
-    #
-    def help(self):
-        first = True
-        help = ''
-
-        # Include the command words
-        for i in range(len(p_input_words)):
-            if (first):
-                first = False
-            else:
-                help += ' '
-
-            help += self.m_word_list[i]
-
-        # Then add the description
-        help += ' : ';
-        help += self.m_desc
-
-        # Caller must add his own newline if needed
-        return help
 
