88e50fb0e35489d2f7962e6dcd518815936e2fc0
Daris A Nevil <dnevil@intrusion.com>
2025-03-06 21:59:43 +0000

    Fixed issue where servo moved at full speed to initial locaiton

--- python/Semaphore.py
+++ python/Semaphore.py
@@ -123,7 +123,10 @@ class Semaphore:
         self.m_pwm_target = self.m_degrees_90_pwm
         self.m_angle_target = 90
         self.m_gpio_pin = GPIO.GPIO("Semaphore", self.m_gpio_id, Pin.OUT, None, self.m_log)
-        self.m_servo = PWM(self.m_gpio_id, freq=pwm_freq, duty=self.m_pwm_duty)
+
+        # Read the current PWM duty, so we can smoothly transition to initial position
+        self.m_servo = PWM(self.m_gpio_id, freq=pwm_freq)
+        self.m_pwm_duty = self.m_servo.duty()
 
         # Create and start timer
         self.m_timer = machine.Timer(Semaphore.c_timer_id)
